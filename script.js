f92cd579cfc116fe1dca3ba439274fe330f27684
guilhermemadeira1 <161243338+guilhermemadeira1@users.noreply.github.com>
2025-08-10 01:18:59 -0300

    style: adicionei menu, busca e filtro animados.

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
-const menu = document.querySelector("#hamburguer");
-const nav = document.querySelector("#options");
+
+const nav = document.querySelector("nav");
 const searchIcon =  document.querySelector("#search-icon");
+const divSearchInput = document.querySelector("#div-search input");
 const priceRanges = document.querySelectorAll(".price-range-input");
+
+const menuOptions = document.querySelector("#menu-options");
+const hamburgerButton = document.querySelector('#hamburguer-button');
+const filterButton = document.querySelector("#filter-button");
+
+
+hamburgerButton.addEventListener("click", ()=>{
+    console.log(menuOptions);
+    menuOptions.classList.toggle("menu-open");
+    if(menuOptions.classList.contains("menu-open")){
+         hamburgerButton.innerHTML = 'close';
+    }
+    else{
+         hamburgerButton.innerHTML = 'menu';
+    }
+});
+
+filterButton.addEventListener("click", ()=>{
+    const priceMobileDiv = document.querySelector(".price-range-mobile-div");
+    console.log(priceMobileDiv);
+
+    priceMobileDiv.classList.toggle("price-range-mobile-div-open");
+
+    if(priceMobileDiv.classList.contains("price-range-mobile-div-open")){
+        filterButton.innerHTML = 'filter_alt_off';   
+    }
+    else{
+        filterButton.innerHTML = 'filter_alt';
+    }
+});
 
 priceRanges.forEach(input =>{
     const label = document.querySelector(`label[for='${input.id}']`);
@@ -14,22 +45,21 @@ priceRanges.forEach(input =>{
 
 });
 
-nav.style.display = 'none';
+//menuOptions.style.display = 'none';
 
 window.addEventListener("resize", ()=>{
-    nav.style.display = 'none';
+
 });
+
+window.addEventListener("", )
+
 
 searchIcon.addEventListener("click",()=>{
-
+    conso
+    divSearchInput.style.left = '39%';
+    divSearchInput.style.opacity = '100%';
+    divSearchInput.style.width = '55%';
 });
 
-menu.addEventListener("click", ()=>{
-    if(nav.style.display === 'flex'){
-        nav.style.display = 'none';
-    }
-    else{
-        nav.style.display = 'flex';
-    }
-});
+
 
