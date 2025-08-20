4195e74c6651c1ca654ad6a246082fe8ac8a9c32
guilhermemadeira1 <161243338+guilhermemadeira1@users.noreply.github.com>
2025-08-20 15:15:01 -0300

    feat: adicionei renderização de produtos com js. style: adicionei imagens dos produtos. fix: corrigi bug da largura dos produtos na tela de celular

--- script.js
+++ script.js
@@ -8,6 +8,100 @@ const menuOptions = document.querySelector("#menu-options");
 const hamburgerButton = document.querySelector('#hamburguer-button');
 const filterButton = document.querySelector("#filter-button");
 
+const products = [
+    {   
+        name: 'Torradeira elétrica',
+        brand: 'Marca X',
+        category: 'Eletrodoméstico',
+        price: '159,90',
+        image: 'imagens/torradeira.jpg'
+    },
+    {
+        name: 'Kit 10 xícaras de porcelana',
+        brand: 'Marca Y',
+        category: 'Cozinha',
+        price: '119,90',
+        image: 'imagens/xicaras.jpg'
+    },
+    {
+        name: 'Ferro de passar roupa',
+        brand: 'Marca X',
+        category: 'Eletrodoméstico',
+        price: '148,00',
+        image: 'imagens/ferro-de-roupa.jpg'
+    },
+    {
+        name: 'Travesseiro de cama',
+        brand: 'Marca Z',
+        category: 'Quarto',
+        price: '49,90',
+        image: 'imagens/travesseiros.jpg'
+    },
+    {
+        name: 'Bicicleta elétrica',
+        brand: 'Marca Z',
+        category: 'Lazer',
+        price: '3.429,90',
+        image: 'imagens/bicicleta-eletrica.jpg'
+    }
+];
+
+products.forEach(prod =>{
+    let name = prod['name'];
+    let brand = prod['brand'];
+    let category = prod['category'];
+    let price = prod['price'];
+    let image = prod['image'];
+
+    if(!prod['image']){
+        image = 'imagens/logo-home.jpg';
+    }
+    createProduct(name,brand,category,price,image);
+});
+
+function createProduct(name, brand, category, price, image){
+    const productSection = document.querySelector('section.product-section');
+    const product = document.createElement('article');
+    const productImg = document.createElement('img');
+    const productDescr = document.createElement('div');
+    const optionsDiv  = document.createElement('div');
+    const btnAdd = document.createElement('button');
+    const btnDetails = document.createElement('button');
+
+    product.setAttribute('class', 'product');
+    productImg.setAttribute('src', image);
+    productImg.setAttribute('alt', 'imagem-produto');
+    productDescr.setAttribute('class','product-description');
+
+    const productName = document.createElement('h3');
+    const brandP  = document.createElement('p');
+    const categoryP  = document.createElement('p');
+    const priceP  = document.createElement('p');
+
+    productName.innerHTML = name;
+    brandP.innerHTML = 'Marca: ' + brand;
+    categoryP.innerHTML = 'Categoria: ' + category;
+    priceP.innerHTML = 'Preço: R$' + price;
+
+    productDescr.appendChild(productName);
+    productDescr.appendChild(brandP);
+    productDescr.appendChild(categoryP);
+    productDescr.appendChild(priceP);
+
+    btnAdd.innerHTML = 'Adicionar';
+    btnAdd.setAttribute('class', 'btn btn-red');
+    btnDetails.innerHTML = 'Detalhes';
+    btnDetails.setAttribute('class', 'btn');
+
+    optionsDiv.appendChild(btnAdd);
+    optionsDiv.appendChild(btnDetails);
+    productDescr.appendChild(optionsDiv);
+
+    product.appendChild(productImg);
+    product.appendChild(productDescr);
+    productSection.appendChild(product);
+}
+
 
 hamburgerButton.addEventListener("click", ()=>{
     console.log(menuOptions);
@@ -42,7 +136,6 @@ priceRanges.forEach(input =>{
     input.addEventListener("change", ()=>{
         label.innerHTML = "Preços até: R$" + input.value + ",00";
     });
-
 });
 
 //menuOptions.style.display = 'none';
@@ -55,7 +148,6 @@ window.addEventListener("", )
 
 
 searchIcon.addEventListener("click",()=>{
-    conso
     divSearchInput.style.left = '39%';
     divSearchInput.style.opacity = '100%';
     divSearchInput.style.width = '55%';
