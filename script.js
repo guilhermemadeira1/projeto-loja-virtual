4ffc3444a47644dbb7247d55767c08f537e0bf06
guilhermemadeira1 <161243338+guilhermemadeira1@users.noreply.github.com>
2025-09-17 18:08:03 -0300

    feat: aba de filtragem por categoria e preço no mobile

--- script.js
+++ script.js
@@ -12,10 +12,14 @@ const priceRangeDesktop = document.querySelector("#price-range-desktop");
 
 const menuOptions = document.querySelector("#menu-options");
 
+const btnClearDesktop = document.querySelector("#btn-clear-desktop");
+const btnClearMobile = document.querySelector("#btn-clear-mobile");
+const categoryOptionsMobile = document.querySelectorAll(".categories-list > .category-option");
+
 const hamburgerButton = document.querySelector('#hamburguer-button');
 const filterIcon = document.querySelector("#filter-icon");
 const filterButtons = document.querySelectorAll(".btn-filter");
-const filterClearButton = document.querySelector("#btn-clear-desktop");
+const filterCleaners = document.querySelectorAll(".btn-clear");
 
 const productSection = document.querySelector("#product-section")
 
@@ -341,10 +345,6 @@ filterButtons.forEach(btn =>{
     });
 });
 
-filterClearButton.addEventListener("click", () => {
-    productSection.innerHTML = "";
-    renderProductSection();
-});
 
 filterIcon.addEventListener("click",()=>{
     const priceMobileDiv = document.querySelector(".price-range-mobile-div");
@@ -359,6 +359,35 @@ filterIcon.addEventListener("click",()=>{
         filterIcon.innerHTML = 'filter_alt';
     }
 });
+
+categoryOptionsMobile.forEach(opt =>{
+    opt.addEventListener("click",()=>{
+        const checkbox = opt.querySelector("input[type='checkbox']");
+        const isChecked = checkbox.checked;
+
+        checkbox.addEventListener("click",()=>{
+            if(checkbox.checked){
+                opt.classList.add("checked-option");
+            }
+            else{
+                opt.classList.remove("checked-option");
+            }
+        });
+        opt.querySelector("input[type='checkbox']") = !isChecked;
+
+        
+        console.log(checkbox.checked);      
+    });
+});
+
+btnClearMobile.addEventListener("click", ()=>{
+    categoryOptionsMobile.forEach(opt =>{
+        productSection.innerHTML = "";
+        renderProductSection();
+        opt.classList.remove("checked-option");
+    });
+});
+
 
 priceRanges.forEach(input =>{ // atualiza o label ao arrastar o range
     const label = document.querySelector(`label[for='${input.id}']`);
